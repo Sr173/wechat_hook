16314eb0a0d54fe222e8763f1a5ed380095f126a
sr173 <66456804@qq.com>
2020-02-05 18:09:24 +0800

    增加了两个hook

--- hook.js
+++ hook.js
@@ -26,19 +26,27 @@ Java.perform(function () {
     var protocal = Java.use("com.tencent.mm.protocal.MMProtocalJni");
     var xlog = Java.use("com.tencent.mars.xlog.Xlog");
     var PByteArray = Java.use("com.tencent.mm.pointers.PByteArray");
+    var sdk = Java.use("oicq.wlogin_sdk.request.b");
 
     xlog.logWrite2.implementation = function(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 ) {
-        //show_java_call_stack();
-        //console.log("ON_LOG", arg8);
+        // show_java_call_stack();
+        // console.log("ON_LOG", arg8);
         return xlog.logWrite2(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
     };
 
-    protocal.pack.implementation = function(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13){
+    protocal.pack.implementation = function(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11){
         show_java_call_stack();
         var tmp = Java.cast(arg1, PByteArray);
-        console.log(toHexString(tmp.value.value));
-        var ret = this.pack(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13);
+        console.log(toHexString(arg0));
+        var ret = this.pack(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
         console.log(toHexString(tmp.value.value));
         return ret
+    };
+
+    sdk.a.implementation = function (arg0, arg1, arg2) {
+        var ret = this.a(arg0, arg1, arg2);
+        console.log(arg2.length, arg2);
+        console.log(toHexString(ret));
+        return ret;
     };
 });
